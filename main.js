3a4cbd1c9cbdd2982005be7cdd879b6d852ee12d
Tim <serazutdinov.tim@gmail.com>
2021-03-21 02:01:46 +0300

    Lesson 5 ended

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'Really nice and soft socks',
             image: './assets/images/socks_green.jpg',
-            amount: 8
+            amount: 8,
+            details: ['20% cotton', '30% wool', '50% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'}
+            ]
         }
     }
 })
